7f0340e551274f6fe930550aaef9f177be4d100c
unknown <muhammedsadeed70@gmail.com>
2022-08-04 19:58:29 +0400

    backend-url

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,7 @@ import {BsInstagram} from 'react-icons/bs'
 function Intro() {
   return (
     <div className='d-flex align-items-center justify-content-center min-vh-100 position-absolute top-50 start-50 translate-middle bg-white z w-100'>
-        <BsInstagram size={125} color={'#fb3958'} style={{
+        <BsInstagram size={110} color={'#fb3958'} style={{
             marginTop: '-9em',
             zIndex: '1',
         }}/>
